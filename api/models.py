24f38b63b9ebcfa36b018af17cdc09282aac1a3c
Андрей <71021948+RenkasVajra@users.noreply.github.com>
2021-01-03 23:08:34 +0500

    Update models.py

--- api/models.py
+++ api/models.py
@@ -5,6 +5,15 @@ from django.db import models
 User = get_user_model()
 
 
+class Group(models.Model):
+    title = models.CharField(
+        max_length=200,
+        )
+
+    def __str__(self):
+        return self.title
+
+    
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
@@ -19,3 +28,24 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     text = models.TextField()
     created = models.DateTimeField("Дата добавления", auto_now_add=True, db_index=True)
+
+
+class Follow(models.Model):
+    following = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following'
+    )
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower'
+    )
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=["user", "following"],
+                name="unique_follow"),
+        )
