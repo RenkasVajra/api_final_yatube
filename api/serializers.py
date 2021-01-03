48b3e27129e5aa119086344b57dedfad7dcf7b1a
Андрей <71021948+RenkasVajra@users.noreply.github.com>
2021-01-03 23:06:49 +0500

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Post, Comment
+from .models import Post, Comment, Group, Follow
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -9,11 +9,54 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('id', 'text', 'author', 'pub_date')
         model = Post
+        
+        
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Group
 
-
+        
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
 
     class Meta:
         fields = ('id', 'author', 'post', 'text', 'created')
         model = Comment
+
+        
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=serializers.CurrentUserDefault(),
+
+    )
+
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all(),
+    )
+
+    def validate_following(self, following):
+        if self.context.get('request').method == 'POST':
+            if self.context['request'].user == following:
+                raise serializers.ValidationError({
+                    'You cant subscribe to yourself'
+                    })
+            if following is None:
+                raise serializer.ValidationError({
+                    'There are no subscriptions per user'
+                    })
+        return following
+
+    class Meta:
+        fields = '__all__'
+        model = Follow
+        validators = (
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following')
+            ),
+        )
+        
